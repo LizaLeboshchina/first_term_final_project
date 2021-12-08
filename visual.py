b617248162cbc3a3e7cf96e6e9c0e8d4960ead0a
ZlatikObr <90772965+ZlatikObr@users.noreply.github.com>
2021-12-09 00:38:33 +0300

    Update visual.py

--- visual.py
+++ visual.py
@@ -1,32 +1,23 @@
-#Экран: 400х480, отступ 80 сверху для подсчета очков, отступы у плиток от границ по 5.
+#обновление графического интерфейса
 
 
-coords = [[[5,85],[105,85],[205,85],[305,85]],     #1 строка плиток
-          [[5,185],[105,185],[205,185],[305,185]],  #2 строка плиток
-          [[5,285],[105,285],[205,285],[305,285]],  #3 строка плиток
-          [[5,385],[105,385],[205,385],[305,385]]]   #4 строка плиток
-
-
-for i in range(4):
-    for j in range (4):
-        bar = Bar(i, j, x, y)
-
-def create_image(bar):
-    bar.image = canv.label(text = str(bar.value),
-                           bg = bar.color,
-                           fg = bar.number.color[bar.value]
-                        ).place(x = bar.x,
-                                y = bar.y
-                            )
-    
 def update_image(bar):
-    bar.image = canv.label(x = bar.x,
-                           y = bar.y,
-                           text = str(bar.value),
-                           bg = bar.color,
-                           fg = bar.number.color[bar.value]
-                        ).place(x = bar.x,
-                                y = bar.y
-                            )
-    
+        for i in range(4):
+            for j in range(4):
+                bar_value = list[i][j]
+                if bar_value == 0:
+                    bars[i][j]["frame"].configure(bg=c.EMPTY_CELL_COLOR)
+                    bars[i][j]["number"].configure(
+                        bg=c.EMPTY_CELL_COLOR, text='')
+                else:
+                    bars[i][j]["frame"].configure(
+                        bg=c.BAR_COLORS[bar_value])
+                    bars[i][j]["number"].configure(
+                        bg=c.BAR_COLORS[cell_value],
+                        fg=c.BAR_NUMBER_COLORS[bar_value],
+                        font=c.BAR_NUMBER_FONTS[bar_value],
+                        text=str(bars.values[i][j]))
+                
+        score_label.configure(text=score)
+        update_idletasks()
     
